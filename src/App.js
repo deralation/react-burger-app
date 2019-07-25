d781c10d74c3d03184daf445541b70e5de568c78
Gokhan Derala <gderala@gmail.com>
2019-07-25 15:20:51 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Layout></Layout>
+        <Layout><p>Test</p></Layout>
       </div>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import Aux from '../../hoc/Aux';
 
 const layout = ( props ) =>(
     <Aux>
-        <div>Toolbar, SideDrawer, Bacldrop</div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
         <main>
             {props.children}
         </main>

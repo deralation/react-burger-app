c9dde287da0fe8089d818ebdd6f98e720eaa1160
Gokhan Derala <gderala@gmail.com>
2019-07-29 14:01:32 +0300

    burger builder added calculation of cost of burger and ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    meat: 1.3,
+    salad: 0.5,
+    cheese: 0.1,
+    bacon: 0.7
+}
+
+
 class BurgerBuilder extends Component {
     //constructor(props){
     //    super();
@@ -14,8 +22,30 @@ class BurgerBuilder extends Component {
             salad:0,
             cheese: 0,
             bacon: 0
-        }
+        },
+        totalPrice: 4
     }
+    // Add ingredient and update the burger
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount+1;
+        const updatedIngrediendts = {
+            ...this.state.ingredients
+        }
+        updatedIngrediendts[type] = updatedCount;
+
+        const priceAddition = INGREDIENT_PRICES[type]
+
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngrediendts});
+    }
+
+    updatedINgredients = (type) => {
+
+    }
+
     render(){
         return(
             <Aux>

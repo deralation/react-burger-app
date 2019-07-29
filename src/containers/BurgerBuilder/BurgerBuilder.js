244dd5769d1a61eef5dd76f9b5cc7e33404c3c9a
Gokhan Derala <gderala@gmail.com>
2019-07-29 11:40:25 +0300

    build control under build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
-import BuildController from '../../components/Burger/BuildControls/BuildControls';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     //constructor(props){
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildController label={}></BuildController>
+                <BuildControls/>
             </Aux>
         );
     }

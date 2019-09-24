a22b2193f5d26a1f869bdff76b043bc45e9cdd94
gokhan derala <gderala@gmail.com>
2019-09-24 23:35:07 +0300

    chanhss

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from './../components/UI/Modal/Modal';
 
 // Ingredients Prices
 const INGREDIENT_PRICES = {

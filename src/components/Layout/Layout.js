18daf045081b7d66abc3020e25047ddbfb170f8b
Gokhan Derala <gderala@gmail.com>
2019-10-08 23:38:30 +0300

    changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,16 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import './Layout.css';
 
-const layout = ( props ) =>(
-    <Aux>
-        <Toolbar/>
-        <SideDrawer />
-        <main className="Content">
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout extends Component {
+    return(
+        <Aux>
+            <Toolbar />
+            <SideDrawer />
+            <main className="Content">
+                {props.children}
+            </main>
+        </Aux>
+    );
+}
 
 export default layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import "./SideDrawer.css";
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props)=>{
 
     return (
-        <div className="SideDrawer">
-            <Logo height="11%"/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div className="SideDrawer">
+                <Logo height="11%"/>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 

fc988cb1f2523127110b72118e06be712e65005f
gokhan derala <gderala@gmail.com>
2019-10-09 00:17:46 +0300

    backdro of sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import './Layout.css';
 
-class layout extends Component {
-    return(
-        <Aux>
-            <Toolbar />
-            <SideDrawer />
-            <main className="Content">
-                {props.children}
-            </main>
-        </Aux>
-    );
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    render () {
+        return(
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className="Content">
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props)=>{
+    let attachedClasses = ['SideDrawer','Close'];
+
+    if(props.open){
+        attachedClasses = ['SideDrawer','Open'];
+    }
 
     return (
         <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-            <div className="SideDrawer">
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
                 <Logo height="11%"/>
                 <nav>
                     <NavigationItems/>
